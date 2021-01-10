9bbe8ab8f69d7a6f803c4c2fa9144a7a08a2050f
Nick <nicholas.roy12@gmail.com>
2021-01-10 12:46:18 +0000

    Organising separate css file

--- src/App.vue
+++ src/App.vue
@@ -21,41 +21,6 @@ export default {
 </script>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,600;1,600&display=swap');
-
-:root {
-  // Colours
-  --c-primary: #474747;
-  --c-secondary: #F8FFF4;
-  --c-accent: #87BBA2;
-
-  // Background
-  --bg-primary: #212121;
-  
-  // Spacers
-  --spacer-xxs: 4px;
-  --spacer-xs: 8px;
-  --spacer-sm: 16px;
-  --spacer-base: 24px;
-  --spacer-lg: 32px;
-  --spacer-xl: 48px;
-  --spacer-2xl: 64px;
-  --spacer-3xl: 128px;
-
-  // Fonts
-  --font-primary: 'Lato', sans-serif;
-  --font-header: 'Raleway', sans-serif;
-
-  // &Font-size
-  --font-xxs: 8px;
-  --font-xs: 12px;
-  --font-sm: 16px;
-  --font-base: 20px;
-  --font-lg: 24px;
-  --font-xl: 28px;
-  --font-2xl: 32px;
-}
 
 #app {
   font-family: var(--font-primary);
