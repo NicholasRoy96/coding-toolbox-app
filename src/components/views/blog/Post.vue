99a87239555a20d2c94cbf10a6d3ac851f606f73
Nick <nicholas.roy12@gmail.com>
2021-02-28 22:46:07 +0000

    Starting on author card

--- src/components/views/blog/Post.vue
+++ src/components/views/blog/Post.vue
@@ -21,6 +21,23 @@
       </div>
       <prismic-rich-text :field="content" :htmlSerializer="HTMLSerializer" class="blog-post__text" />
     </div>
+    <div class="blog-post__author-card flex flex-row justify-center items-center">
+      <div class="blog-post__author-card__image flex justify-center items-center">
+        <img class="mx-6" width="40" src="@/assets/avatars/boy-1.svg" />
+      </div>
+      <div class="blog-post__author-card__content">
+        <p class="blog-post__author-card__content__description mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, a obcaecati mollitia explicabo velit quasi voluptatum delectus facilis dolorem perferendis officiis nam optio quia sequi. Odio corporis quibusdam excepturi sunt!</p>
+        <div class="flex justify-between items-center">
+          <div class="blog-post__author-card__content__links flex">
+            <img src="@/assets/icons/github.svg" width="25">
+            <img src="@/assets/icons/youtube.svg" width="25">
+          </div>
+          <button class="blog-post__author-card__content__button">
+            More By This Author
+          </button>
+        </div>
+      </div>
+    </div>
     <div class="blog-post__back-bar">
       <button @click="$router.push('/')" class="blog-post__back-bar__button">
         Back to Blogs
@@ -209,6 +226,36 @@ export default {
       }
     }
   }
+  &__author-card {
+    display: flex;
+    justify-content: flex-end;
+    position: relative;
+    max-width: 758px;
+    margin: 0 auto;
+    border-radius: 10px;
+    background: var(--c-offwhite);
+    box-shadow: 0px 0px 8px 3px rgba(0, 0, 0, 0.1);
+    &__image {
+      position: absolute;
+      left: 0;
+      top: 0;
+      bottom: 0;
+      width: 20%;
+      background: var(--c-lightred);
+      border-top-left-radius: 10px;
+      border-bottom-left-radius: 10px;
+    }
+    &__content {
+      width: 80%;
+      text-align: start;
+      padding: var(--spacer-lg);
+      p {
+        font-family: var(--font-secondary);
+        font-size: var(--font-sm);
+        color: var(--c-lightgrey);
+      }
+    }
+  }
   &__back-bar {
     width: 100%;
     background: rgba(255, 153, 1, 0.08);
@@ -269,6 +316,7 @@ export default {
       margin: var(--spacer-lg) 0;
       padding: var(--spacer-lg);
       font-size: var(--font-sm);
+      border-radius: 2px;
       overflow-x: scroll;
     }
   }
